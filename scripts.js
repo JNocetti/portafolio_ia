6935a9d9712e06a27c6ba7607878b50bbda55814
JNocetti <juan.anocetti@gmail.com>
2024-09-23 23:53:49 -0300

    landing page updated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll('.container, .carousel, footer');
     elements.forEach(element => {
@@ -7,14 +6,5 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(() => {
             element.style.opacity = 1;
         }, 200);
-    });
-
-    const mainImage = document.querySelector('.main-image');
-    mainImage.addEventListener('mouseover', () => {
-        mainImage.style.transform = "scale(1.05)";
-        mainImage.style.transition = "transform 0.3s ease";
-    });
-    mainImage.addEventListener('mouseout', () => {
-        mainImage.style.transform = "scale(1)";
     });
 });
