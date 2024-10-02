5ccdc62fd83b5808a283736c38ac644cdc30fbf6
JNocetti <juan.anocetti@gmail.com>
2024-10-01 21:31:12 -0300

    landing page v1

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll('.container, .carousel, footer');
-    elements.forEach(element => {
-        element.style.opacity = 0;
-        element.style.transition = "opacity 1.5s ease";
-        setTimeout(() => {
-            element.style.opacity = 1;
-        }, 200);
-    });
+  const elements = document.querySelectorAll('.container, .carousel, footer');
+  elements.forEach(element => {
+      element.style.opacity = 0;
+      element.style.transition = "opacity 1.5s ease";
+      setTimeout(() => {
+          element.style.opacity = 1;
+      }, 200);
+  });
 });
